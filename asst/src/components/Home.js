ffef266a4080a9eb002e8c4f24adeef0f6c4cc48
Michael O'Sullivan <2402564+heavymossman@users.noreply.github.com>
2022-12-19 01:00:14 +0000

    homepage cards, and carsouel

--- asst/src/components/Home.js
+++ asst/src/components/Home.js
@@ -1,11 +1,48 @@
-import React from "react"
-import { Component } from "react"
+import React from "react";
+import { Component } from "react";
+import Display from "./Display"; 
+import { Container, Row, Col, Carousel } from "react-bootstrap";
+import cimage1 from '../cimage1.png'
 
 class Home extends Component {
     render() {
         return (
             <div>
-                <h1>This is the homepage</h1>
+                
+         
+                <Carousel>
+                    <Carousel.Item>
+                        <img className="d-block w-100" src={cimage1} alt="Whatever we use" />
+                    </Carousel.Item>
+                </Carousel>
+                <br /><br />
+            
+                <Container>
+            <Row>
+                <Col>
+                    <Display header="Xmas Card Writer"
+                    title="Title one goes here"
+                    text="The text for card 1 goes here"
+                    theLink="/xmas-card-writer" />
+                </Col>
+                <Col>
+                    <Display header="Article Writer"
+                    title="Title one goes here"
+                    text="The text for card 1 goes here"
+                    theLink="/xmas-card-writer" />
+                </Col>
+                <Col>
+                    <Display header="Tweet Writer"
+                    title="Title one goes here"
+                    text="The text for card 1 goes here"
+                    theLink="/xmas-card-writer" />
+                </Col>
+            </Row>
+
+
+
+
+            </Container>
             </div>
         )
     }
