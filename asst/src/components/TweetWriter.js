31734e7e8f37835116ec0ee2cfb2c34d4f70c41a
Michael O'Sullivan <2402564+heavymossman@users.noreply.github.com>
2022-12-20 20:43:30 +0000

    package and pre reinstall all plugins

--- asst/src/components/TweetWriter.js
+++ asst/src/components/TweetWriter.js
@@ -7,21 +7,25 @@ const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
 });
 
+const openai = new OpenAIApi(configuration);
 
 
 
 class Tweet extends Component {
 
-    
-    
 
     render() {
+        
+
+        
         return (
             <div>
                 
                 <h1>This is the Tweet Writer</h1>
             </div>
         )
+
+        
         
     }
 }
