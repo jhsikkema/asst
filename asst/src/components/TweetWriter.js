bb2095b98f617f94e450b097ed4fcb6542aad93a
Michael O'Sullivan <2402564+heavymossman@users.noreply.github.com>
2022-12-20 22:29:00 +0000

    new tweet writer page with cnstructor

--- asst/src/components/TweetWriter.js
+++ asst/src/components/TweetWriter.js
@@ -1,39 +1,50 @@
 import React from "react"
 import { Component } from "react"
+import axios from 'axios';
 const { Configuration, OpenAIApi } = require('openai');
-
+const apiKey = process.env.OPENAI_API_KEY;
 
 const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
 });
 
-const openai = new OpenAIApi(configuration);
-
-
-
 
 class Tweet extends Component {
-    
-    
-    
-
-
-
-
-    render() {
-        
-        
-        
-        return (
-            <div>
-                
-                <h1>This is the Tweet Writer</h1>
-            </div>
-        )
-
-        
-        
+    state = {
+      completion: ''
     }
-}
-
-export default Tweet;
+  
+    componentDidMount() {
+      const data = {
+        model: 'text-davinci-003',
+        prompt: 'Write a short poem about Michael Moss',
+        temperature: 0,
+        max_tokens: 7,
+      };
+      
+      axios.post('https://api.openai.com/v1/completions', data, {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: 'Bearer' + apiKey,
+        },
+      })
+        .then(response => {
+          // handle success
+          this.setState({ completion: response.data })
+        })
+        .catch(error => {
+          // handle error
+          console.log(error);
+        });
+    }
+  
+    render() {
+      return (
+        <div>
+          {this.state.completion}
+        </div>
+      )
+    }
+  }
+  
+  export default Tweet;
